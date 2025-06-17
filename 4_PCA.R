636b859ed3a84654e8cfe58331fe7d3372e2ef8e
soleopold <leopold@unhcr.org>
2025-06-17 13:49:42 +0200

    Update 4_PCA.R

--- 4_PCA.R
+++ 4_PCA.R
@@ -1,5 +1,14 @@
 main <- read.csv("C:/Users/LEOPOLD/OneDrive - UNHCR/Work/DHS Wealth index/main.csv")
 
+main <- main %>%
+  mutate(
+    urban_bin = case_when(
+      Intro_09 == 3 ~ 1, 
+      TRUE ~ 0))
+
+table(main$urban_bin)
+
+
 #select all variables for final dataset
 main_WI <- main %>%
   select(
@@ -9,8 +18,9 @@ main_WI <- main %>%
     Intro_07_1,
     Region,
     group,
+    urban_bin,
     Improved_dw,             # Improved drinking water
-#    Improved_dw_final,      # Improved drinking water, <30 min
+    #    Improved_dw_final,      # Improved drinking water, <30 min
     electricity,            # Has electricity
     shared_san_facility,     # Shared toilet facility?
     Improved_san,            # Type of toilet facility
@@ -21,7 +31,7 @@ main_WI <- main %>%
     House01,
     HH_30a,
     
-        # Housing materials
+    # Housing materials
     HH02_finished,
     HH03_finished,
     HH06_finished,
@@ -32,85 +42,92 @@ main_WI <- main %>%
     HH03_natural,
     HH06_natural,
     
-  #Assets
-#    starts_with("Assets")
-    Assets01, #Radio
-    Assets01aa,	#Satellite dish
-    Assets01b,	#Television
-    Assets01bb,	#Seeds for planting
-    Assets01c,	#Table phone
-    Assets01cc,	#Plough (oxen)
-    Assets01d,	#Refrigerator
-    Assets01dd,	#Diesel driven irrigation system
-    Assets01e,	#CD/DVD player
-    Assets01ee,	#Hoe
-    Assets01f,	#Fan
-    Assets01ff,	#Maloda
-    Assets01g,	#Air conditioner
-    Assets01gg,	#Spade
-    Assets01h,	#Stove/kanun (traditional)
-    Assets01hh,	#Axe
-    Assets01i,	#Gas/kerosene cooker
-    Assets01ii,	#Panga
-    Assets01j,	#Grinding stone
-    Assets01jj,	#Spraying cans
-    Assets01k,	#Mixer/blender
-    Assets01kk,	#Harrow
-    Assets01l,	#Generator
-    Assets01ll,	#Ridger
-    Assets01m,	#Solar panel (any size)
-    Assets01mm,	#Cultivators
-    Assets01n,	#Wall clock
-    Assets01nn,	#Spreaders
-    Assets01o,	#Modem/router
-    Assets01q,	#Water pump
-    Assets01r,	#Chair
-    Assets01s_a,#Mattress
-    Assets01s_b,#Bed
-    Assets01t,  #Sofa
-    Assets01u,	#Room-cooler
-    Assets01v,	#Washing machine
-    Assets01w,	#Camera, other than phone
-    Assets01x,	#Sewing machine
-    Assets01y,	#Tools for lighting (solar lamp, torch etc.)
-    Assets01z,	#Freezer
-
-  #Working assets
-    Assets02a,	#Watch
-    Assets02b,	#Mobile phone
-    Assets02c,	#Smart phone
-    Assets02d,  #Bicycle
-    Assets02e,  #Motorcycle or motor scooter
-    Assets02f_a,#Car
-    Assets02f_b,#Truck
-    Assets02g,  #Boat without motor/canoe
-    Assets02j,  #Tractor
-    Assets02l,  #Laptop/computer
-    Assets02n,  #Pair of shoes
-
-  #Animals
-     Assets04a_1, #Cows/bulls: 1-4
-     Assets04a_2, #Cows/bulls: 5-9
-     Assets04a_3, #Cows/bulls: 10+
-     Assets04b_1, #Other cattle: 1-4
-     Assets04b_2, #Other cattle: 5-9
-     Assets04b_3, #Other cattle: 10+
-     Assets04c_1, #Horses/donkeys/mules: 1-4
-     Assets04c_2, #Horses/donkeys/mules: 5-9
-     Assets04c_3, #Horses/donkeys/mules: 10+
-     Assets04d_1, #Goats: 1-4
-     Assets04d_2, #Goats: 5-9
-     Assets04d_3, #Goats: 10+
-     Assets04e_1, #Sheep: 1-4
-     Assets04e_2, #Sheep: 5-9
-     Assets04e_3, #Sheep: 10+
-     Assets04f_1, #Barnyard animals: 1-4
-     Assets04f_2, #Barnyard animals: 5-9
-     Assets04f_3, #Barnyard animals: 10+
-     Assets04g_1, #Pigs: 1-4
-     Assets04g_2, #Pigs: 5-9
-     Assets04g_3  #Pigs: 10+
+    #Assets
+    starts_with("Assets01"),
+    starts_with("Assets02"),
+    starts_with("Assets04")
   )
+
+
+main_WI_urban <- main %>%
+  filter(urban_bin == 1) %>%
+  select(
+    # Core service access variables
+    ID,
+    wgh_samp_resc_str,
+    Intro_07_1,
+    Region,
+    group,
+    urban_bin,
+    Improved_dw,             # Improved drinking water
+    #    Improved_dw_final,      # Improved drinking water, <30 min
+    electricity,            # Has electricity
+    shared_san_facility,     # Shared toilet facility?
+    Improved_san,            # Type of toilet facility
+    crowding_cat,            # Overcrowding index
+    Clean_cookingfuel,       # Clean cooking fuel
+    Land01,
+    agricultural_land_ha,
+    House01,
+    HH_30a,
+    
+    # Housing materials
+    HH02_finished,
+    HH03_finished,
+    HH06_finished,
+    HH02_rudimentary,
+    HH03_rudimentary,
+    HH06_rudimentary,
+    HH02_natural,
+    HH03_natural,
+    HH06_natural,
+    
+    #Assets
+    starts_with("Assets01"),
+    starts_with("Assets02"),
+    starts_with("Assets04")
+  )
+
+
+main_WI_rural <- main %>%
+  filter(urban_bin == 0) %>%
+  select(
+    # Core service access variables
+    ID,
+    wgh_samp_resc_str,
+    Intro_07_1,
+    Region,
+    group,
+    urban_bin,
+    Improved_dw,             # Improved drinking water
+    #    Improved_dw_final,      # Improved drinking water, <30 min
+    electricity,            # Has electricity
+    shared_san_facility,     # Shared toilet facility?
+    Improved_san,            # Type of toilet facility
+    crowding_cat,            # Overcrowding index
+    Clean_cookingfuel,       # Clean cooking fuel
+    Land01,
+    agricultural_land_ha,
+    House01,
+    HH_30a,
+    
+    # Housing materials
+    HH02_finished,
+    HH03_finished,
+    HH06_finished,
+    HH02_rudimentary,
+    HH03_rudimentary,
+    HH06_rudimentary,
+    HH02_natural,
+    HH03_natural,
+    HH06_natural,
+    
+    #Assets
+    starts_with("Assets01"),
+    starts_with("Assets02"),
+    starts_with("Assets04")
+  )
+
 
 
 
@@ -119,68 +136,203 @@ str(main_WI)
 
 
 #remove variables with zero variation
-zero_sd_vars_main <- sapply(main_WI[ , 6:ncol(main_WI)], function(x) sd(x, na.rm = TRUE) == 0)
-removed_vars_main <- names(main_WI)[which(zero_sd_vars_main) + 5]  # +5 to account for starting at column 5
+zero_sd_vars_main <- sapply(main_WI[ , 7:ncol(main_WI)], function(x) sd(x, na.rm = TRUE) == 0)
+removed_vars_main <- names(main_WI)[which(zero_sd_vars_main) + 6]  # +5 to account for starting at column 5
 main_WI <- main_WI[ , !(names(main_WI) %in% removed_vars_main)]
 cat("Removed variables from main:", removed_vars_main, "\n")
 
 
+zero_sd_vars_urban <- sapply(main_WI_urban[ , 7:ncol(main_WI_urban)], function(x) sd(x, na.rm = TRUE) == 0)
+removed_vars_urban <- names(main_WI_urban)[which(zero_sd_vars_urban) + 6]  # +6 to account for starting at column 5
+main_WI_urban <- main_WI_urban[ , !(names(main_WI_urban) %in% removed_vars_urban)]
+cat("Removed variables from urban:", removed_vars_urban, "\n")
+
+
+zero_sd_vars_rural <- sapply(main_WI_rural[ , 7:ncol(main_WI_rural)], function(x) sd(x, na.rm = TRUE) == 0)
+removed_vars_rural <- names(main_WI_rural)[which(zero_sd_vars_rural) + 6]  # +6 to account for starting at column 5
+main_WI_rural <- main_WI_rural[ , !(names(main_WI_rural) %in% removed_vars_rural)]
+cat("Removed variables from rural:", removed_vars_rural, "\n")
+
+
 
 #PCA
-pca_main <- principal(main_WI[6:ncol(main_WI)], rotate = "varimax", cor = "mixed") 
-print(pca_main$loadings, cutoff = 0, sort=F)
-print(pca_main$scores)
+#calc combined PCA
+main_pca <- psych::principal(
+  main_WI[, 7:ncol(main_WI)],        # select only binary variables
+  rotate = "varimax",           # optional rotation
+  nfactors = 1,                 # number of components
+  covar = TRUE,                 # use covariance matrix (not correlation)
+  cor = "mixed",
+  scores = TRUE                 # return scores
+)
 
-kmo_result <- KMO(main_WI[6:ncol(main_WI)])
-print(kmo_result)
-
-main_WI$scores <- pca_main$scores
 
 
-#Create quintiles based on wealth scores
+# Run PCA for urban dataset
+main_pca_urban <- psych::principal(
+  main_WI_urban[, 7:ncol(main_WI_urban)],        # select only binary variables
+  rotate = "varimax",           # optional rotation
+  nfactors = 1,                 # number of components
+  covar = TRUE,
+  cor = "mixed",
+  scores = TRUE                 # return scores
+)
+
+# Run PCA for rural dataset
+main_pca_rural <- psych::principal(
+  main_WI_rural[, 7:ncol(main_WI_rural)],        # select only binary variables
+  rotate = "varimax",           # optional rotation
+  nfactors = 1,                 # number of components
+  covar = TRUE,                 # use covariance matrix (not correlation)
+  cor = "mixed",
+  scores = TRUE                 # return scores
+)
+
+
+
+#Merge pca scores into main dataset
+main_WI$comscore <- main_pca$scores[,1]
+main_WI$urbscore <- NA
+main_WI$rurscore <- NA
+
+
+main_WI$urbscore[main_WI$urban_bin == 1] <- main_pca_urban$scores[, 1]
+main_WI$rurscore[main_WI$urban_bin == 0] <- main_pca_rural$scores[, 1]
+
+
+
+
+#Run Separate regressions to link urban/rural PCA scores to common pca score
+# Urban regression: comscore ~ urbscore
+urban_model <- lm(comscore ~ urbscore, data = main_WI, subset = urban_bin == 1)
+
+# Rural regression: comscore ~ rurscore
+rural_model <- lm(comscore ~ rurscore, data = main_WI, subset = urban_bin == 0)
+
+# Extract coefficients
+urb_const <- coef(urban_model)[1]
+urb_coeff <- coef(urban_model)[2]
+
+rur_const <- coef(rural_model)[1]
+rur_coeff <- coef(rural_model)[2]
+
+
+#Construct Combined Wealth index (combscore)
+main_WI$combscore <- NA
+main_WI$combscore[main_WI$urban_bin == 1] <- urb_const + urb_coeff * main_WI$urbscore[main_WI$urban_bin == 1]
+main_WI$combscore[main_WI$urban_bin == 0] <- rur_const + rur_coeff * main_WI$rurscore[main_WI$urban_bin == 0]
+
+
+
+#Combined/national quintiles
 main_WI <- main_WI %>%
-  filter(!is.na(group), !is.na(scores)) %>%
+  filter(!is.na(combscore)) %>%
   mutate(
-    wealth_quintile = ntile(scores, 5),
+    q_combscore = ntile(combscore, 5),
+    q_combscore = factor(q_combscore, levels = 1:5,
+                         labels = c("Poorest", "Second", "Middle", "Fourth", "Richest"))
+  )
+
+#Common/Join PCA Quintiles
+main_WI <- main_WI %>%
+  filter(!is.na(comscore)) %>%
+  mutate(
+    wealth_quintile = ntile(comscore, 5),
     wealth_quintile = factor(wealth_quintile, levels = 1:5, 
                              labels = c("Poorest", "Second", "Middle", "Fourth", "Richest"))
   )
 
-# Step 3: Summarize for plotting
-wi_data <- main_WI %>%
-  group_by(group, wealth_quintile) %>%
-  summarise(n = n(), .groups = "drop") %>%
-  group_by(group) %>%
-  mutate(share = n / sum(n)) %>%
-  ungroup()
+main_WI<- main_WI[!is.na(main_WI$combscore), ]
 
-# Ensure correct ordering
-wi_data$wealth_quintile <- factor(
-  table_data$wealth_quintile,
-  levels = c( "Richest", "Fourth", "Middle", "Second", "Poorest")
-)
 
-# Step 4: Plot
-ggplot(wi_data, aes(x = share, y = group, fill = wealth_quintile)) +
-  geom_col(position = "stack", width = 0.5) +  
-  scale_x_continuous(labels = scales::percent_format()) +
-  scale_fill_manual(
-    values = c(
-      "Poorest" = "#666666",
-      "Second" = "#00B398",
-      "Middle" = "#8EBEFF",
-      "Fourth" = "#0072BC",
-      "Richest" = "#18375F"
+
+
+
+
+#IF RUNNING PCA JOINTLY FOR URBAN AND RURAL
+main_WI %>%
+  filter(group %in% c("Refugees South", "Refugees North", "Host Community North"),
+         !is.na(wealth_quintile)) %>%
+  mutate(wealth_quintile = fct_rev(wealth_quintile)) %>%  # Reverse order
+  ggplot(aes(x = group, fill = wealth_quintile)) +
+  geom_bar(position = "fill") +
+  xlab("Group") +
+  ylab("Percentage") +
+  ggtitle("Wealth Distribution by Group") +
+  scale_y_continuous(labels = scales::percent_format()) +
+  scale_fill_unhcr_d(guide = guide_legend(reverse = TRUE)) +  # Reverse legend
+  theme_unhcr() +
+  coord_flip()
+
+main_WI %>%
+  filter(Intro_07_1 %in% c(1, 3), !is.na(wealth_quintile)) %>%
+  mutate(
+    population_group = case_when(
+      Intro_07_1 == 1 ~ "Refugees",
+      Intro_07_1 == 3 ~ "Host Community"
     )
-    ,   guide = guide_legend(reverse = TRUE)
-  ) +
-  labs(
-    x = "Share of Households",
-    y = "",
-    fill = "Wealth Quintile",
-    title = "Wealth Quintile Distribution by Group"
-  ) +
-  theme_unhcr()
+  ) %>%
+  mutate(wealth_quintile = fct_rev(wealth_quintile)) %>%  # Reverse order
+  ggplot(aes(x = population_group, fill = wealth_quintile)) +
+  geom_bar(position = "fill") +
+  xlab("Population Group") +
+  ylab("Percentage") +
+  ggtitle("Wealth Distribution: Refugees vs. Host Community") +
+  scale_y_continuous(labels = scales::percent_format()) +
+  scale_fill_unhcr_d(guide = guide_legend(reverse = TRUE)) +  # Reverse legend
+  theme_unhcr() +
+  coord_flip()
 
-# Step 5: Save plot
-ggsave("C:/Users/LEOPOLD/OneDrive - UNHCR/Work/DHS Wealth index/figures/quintiles.png", width = 6, height = 4, dpi = 300)
+
+
+#RUNNING URBAN/RURAL SEPARATELY
+
+
+main_WI %>%
+  filter(group %in% c("Refugees South", "Refugees North", "Host Community North"),
+         !is.na(q_combscore)) %>%
+  mutate(q_combscore = fct_rev(q_combscore)) %>%  # Reverse order
+  ggplot(aes(x = group, fill = q_combscore)) +
+  geom_bar(position = "fill") +
+  xlab("Group") +
+  ylab("Percentage") +
+  ggtitle("Wealth Distribution by Group") +
+  scale_y_continuous(labels = scales::percent_format()) +
+  scale_fill_unhcr_d(guide = guide_legend(reverse = TRUE)) +  # Use UNHCR color palette
+  theme_unhcr() +
+  coord_flip()
+
+
+main_WI %>%
+  filter(Intro_07_1 %in% c(1, 3), !is.na(q_combscore)) %>%
+  mutate(
+    population_group = case_when(
+      Intro_07_1 == 1 ~ "Refugees",
+      Intro_07_1 == 3 ~ "Host Community"
+    )
+  ) %>%
+  mutate(q_combscore = fct_rev(q_combscore)) %>%  # Reverse order
+  ggplot(aes(x = population_group, fill = q_combscore)) +
+  geom_bar(position = "fill") +
+  xlab("Population Group") +
+  ylab("Percentage") +
+  ggtitle("Wealth Distribution: Refugees vs. Host Community") +
+  scale_y_continuous(labels = scales::percent_format()) +
+  scale_fill_unhcr_d(guide = guide_legend(reverse = TRUE)) +  # Use UNHCR color palette
+  theme_unhcr() +
+  coord_flip()
+
+
+
+
+
+
+
+
+
+
+#Inspect loadings
+print(main_pca$loadings, cutoff = 0, sort=F)
+print(main_pca_urban$loadings, cutoff = 0, sort=F)
+print(main_pca_rural$loadings, cutoff = 0, sort=F)
+
