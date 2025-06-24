2f64305718cf107ce72609247cd49a5a5b9e49f4
soleopold <leopold@unhcr.org>
2025-06-24 15:31:52 +0200

    Update WI_PAK.R

--- WI_PAK.R
+++ WI_PAK.R
@@ -162,6 +162,7 @@ main_WI <- main %>%
     wgh_strata_spec,
     samp_strat,
     Intro_09,
+    Intro_08,
     Intro_07,
     Improved_dw,             # Improved drinking water
     Improved_dw_final,      # Improved drinking water, <30 min
@@ -202,6 +203,7 @@ main_WI_urban <- main %>%
     wgh_strata_spec,
     samp_strat,
     Intro_09,
+    Intro_08,
     Intro_07,
     Improved_dw,             # Improved drinking water
     Improved_dw_final,      # Improved drinking water, <30 min
@@ -243,6 +245,7 @@ main_WI_rural <- main %>%
     wgh_strata_spec,
     samp_strat,
     Intro_09,
+    Intro_08,
     Intro_07,
     Improved_dw,             # Improved drinking water
     Improved_dw_final,      # Improved drinking water, <30 min
@@ -274,112 +277,34 @@ main_WI_rural <- main %>%
 
 
 #remove variables with zero variation
-zero_sd_vars_main <- sapply(main_WI[ , 7:ncol(main_WI)], function(x) sd(x, na.rm = TRUE) == 0)
-removed_vars_main <- names(main_WI)[which(zero_sd_vars_main) + 6]  # +6 to account for starting at column 5
+zero_sd_vars_main <- sapply(main_WI[ , 8:ncol(main_WI)], function(x) sd(x, na.rm = TRUE) == 0)
+removed_vars_main <- names(main_WI)[which(zero_sd_vars_main) + 7]  # +6 to account for starting at column 5
 main_WI <- main_WI[ , !(names(main_WI) %in% removed_vars_main)]
 cat("Removed variables from main:", removed_vars_main, "\n")
 
-zero_sd_vars_urban <- sapply(main_WI_urban[ , 7:ncol(main_WI_urban)], function(x) sd(x, na.rm = TRUE) == 0)
-removed_vars_urban <- names(main_WI_urban)[which(zero_sd_vars_urban) + 6]  # +6 to account for starting at column 5
+zero_sd_vars_urban <- sapply(main_WI_urban[ , 8:ncol(main_WI_urban)], function(x) sd(x, na.rm = TRUE) == 0)
+removed_vars_urban <- names(main_WI_urban)[which(zero_sd_vars_urban) + 7]  # +6 to account for starting at column 5
 main_WI_urban <- main_WI_urban[ , !(names(main_WI_urban) %in% removed_vars_urban)]
 cat("Removed variables from urban:", removed_vars_urban, "\n")
 
 
-zero_sd_vars_rural <- sapply(main_WI_rural[ , 7:ncol(main_WI_rural)], function(x) sd(x, na.rm = TRUE) == 0)
-removed_vars_rural <- names(main_WI_rural)[which(zero_sd_vars_rural) + 6]  # +6 to account for starting at column 5
+zero_sd_vars_rural <- sapply(main_WI_rural[ , 8:ncol(main_WI_rural)], function(x) sd(x, na.rm = TRUE) == 0)
+removed_vars_rural <- names(main_WI_rural)[which(zero_sd_vars_rural) + 7]  # +6 to account for starting at column 5
 main_WI_rural <- main_WI_rural[ , !(names(main_WI_rural) %in% removed_vars_rural)]
 cat("Removed variables from rural:", removed_vars_rural, "\n")
 
 
 
-#Remove variables that are not present in one group for all groups.
-#main_WI <- main_WI%>%
-#  select(-c(Assets01j, Assets01m, Assets01o, 
-#            Assets02f_b, Assets02g, Assets02h, Assets02i, 
-#            Assets04a_1, Assets04a_2, Assets04a_3, 
-#            Assets04b_2, Assets04b_3, Assets04c_1, Assets04c_2, 
-#            Assets04c_3, Assets04d_1, Assets04d_2, Assets04d_3, 
-#            Assets04e_2, Assets04e_3, Assets04f_2, Assets04g_1, Assets04g_2, Assets04g_3))
-
-
 ###WI is calculated by using PCA (principal component analysis)
-
-
 #calc combined PCA
 main_pca <- psych::principal(
-  main_WI[, 7:ncol(main_WI)],        # select only binary variables
+  main_WI[, 8:ncol(main_WI)],        # select only binary variables
   rotate = "varimax",           # optional rotation
   nfactors = 1,                 # number of components
   covar = TRUE,                 # use covariance matrix (not correlation)
   cor = "tet",
   scores = TRUE                 # return scores
 )
-
-
-
-# Run PCA for urban dataset
-main_pca_urban <- psych::principal(
-  main_WI_urban[, 7:ncol(main_WI_urban)],        # select only binary variables
-  rotate = "varimax",           # optional rotation
-  nfactors = 1,                 # number of components
-  covar = TRUE,
-  cor = "tet",
-  scores = TRUE                 # return scores
-)
-
-# Run PCA for rural dataset
-main_pca_rural <- psych::principal(
-  main_WI_rural[, 7:ncol(main_WI_rural)],        # select only binary variables
-  rotate = "varimax",           # optional rotation
-  nfactors = 1,                 # number of components
-  covar = TRUE,                 # use covariance matrix (not correlation)
-  cor = "tet",
-  scores = TRUE                 # return scores
-)
-
-
-
-#Merge pca scores into main dataset
-main_WI$comscore <- main_pca$scores[,1]
-main_WI$urbscore <- NA
-main_WI$rurscore <- NA
-
-
-main_WI$urbscore[main_WI$urref_bin == 1] <- main_pca_urban$scores[, 1]
-main_WI$rurscore[main_WI$urref_bin == 0] <- main_pca_rural$scores[, 1]
-
-
-
-
-#Run Separate regressions to link urban/rural PCA scores to common pca score
-
-# Urban regression: comscore ~ urbscore
-urban_model <- lm(comscore ~ urbscore, data = main_WI, subset = urref_bin == 1)
-
-# Rural regression: comscore ~ rurscore
-rural_model <- lm(comscore ~ rurscore, data = main_WI, subset = urref_bin == 0)
-
-# Extract coefficients
-urb_const <- coef(urban_model)[1]
-urb_coeff <- coef(urban_model)[2]
-
-rur_const <- coef(rural_model)[1]
-rur_coeff <- coef(rural_model)[2]
-
-
-#Construct Combined Wealth index (combscore)
-main_WI$combscore <- NA
-main_WI$combscore[main_WI$urref_bin == 1] <- urb_const + urb_coeff * main_WI$urbscore[main_WI$urref_bin == 1]
-main_WI$combscore[main_WI$urref_bin == 0] <- rur_const + rur_coeff * main_WI$rurscore[main_WI$urref_bin == 0]
-
-#Combined/national quintiles
-main_WI <- main_WI %>%
-  filter(!is.na(combscore)) %>%
-  mutate(
-    q_combscore = ntile(combscore, 5),
-    q_combscore = factor(q_combscore, levels = 1:5,
-                         labels = c("Poorest", "Second", "Middle", "Fourth", "Richest"))
-  )
 
 #Common/Join PCA Quintiles
 main_WI <- main_WI %>%
@@ -390,66 +315,42 @@ main_WI <- main_WI %>%
                              labels = c("Poorest", "Second", "Middle", "Fourth", "Richest"))
   )
 
-main_WI<- main_WI[!is.na(main_WI$combscore), ]
 
 
-###Create a ggplot group by regions
-#IF RUNNING URBAN AND RURAL SEPARATELY AND BRINGING TOGETHER USING REGRESSIONS
+
+
+#using joint calculation & comparing refugees in and out of camp and host community
 main_WI %>%
-  filter(!is.na(q_combscore)) %>%
-  mutate(q_combscore = fct_rev(q_combscore)) %>%  # Reverse order
-  ggplot(aes(x = samp_strat, fill = q_combscore)) +
+  filter(Intro_07 == "Refugees" | Intro_07 == "Host community", !is.na(wealth_quintile)) %>%
+  mutate(
+    pop_group = case_when(
+      Intro_07 == "Host community"                         ~ "Host community",
+      Intro_07 == "Refugees" & Intro_08 == 1              ~ "Refugees (in-camp)",
+      Intro_07 == "Refugees" & Intro_08 == 2              ~ "Refugees (out-of-camp)",
+      TRUE                                                 ~ NA_character_
+    ),
+    pop_group = factor(pop_group, levels = c(
+      "Host community",
+      "Refugees (out-of-camp)",
+      "Refugees (in-camp)"
+    )),
+    wealth_quintile = fct_rev(wealth_quintile)  # Reverse order (if needed)
+  ) %>%
+  filter(!is.na(pop_group)) %>%
+  ggplot(aes(x = pop_group, fill = wealth_quintile)) +
   geom_bar(position = "fill") +
-  xlab("Province") +
-  ylab("Percentage") +
-  ggtitle("Wealth by Province") +
+  ggtitle("Wealth Distribution FDS Pakistan") +
   scale_y_continuous(labels = scales::percent_format()) +
-  scale_fill_unhcr_d(guide = guide_legend(reverse = TRUE)) +  # Use UNHCR color palette
+  scale_fill_unhcr_d(guide = guide_legend(reverse = TRUE)) +
   theme_unhcr() +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())+
   coord_flip()
 
-main_WI %>%
-  filter(Intro_07 %in% c("Refugees", "Host community"), !is.na(q_combscore)) %>%
-  mutate(q_combscore = fct_rev(q_combscore)) %>%  # Reverse order
-  ggplot(aes(x = Intro_07, fill = q_combscore)) +
-  geom_bar(position = "fill") +
-  xlab("Population Group") +
-  ylab("Percentage") +
-  ggtitle("Wealth Distribution: Refugees vs. Host Community") +
-  scale_y_continuous(labels = scales::percent_format()) +
-  scale_fill_unhcr_d(guide = guide_legend(reverse = TRUE)) +  # Use UNHCR color palette
-  theme_unhcr() +
-  coord_flip()
+ggsave("C:/Users/LEOPOLD/OneDrive - UNHCR/Work/DHS Wealth index/figures/quintiles_PAK.png", width = 6, height = 4, dpi = 300)
 
 
 
 
 
 
-#IF RUNNING PCA JOINTLY FOR URBAN AND RURAL
-main_WI %>%
-  filter(!is.na(wealth_quintile)) %>%
-  mutate(wealth_quintile = fct_rev(wealth_quintile)) %>%  # Reverse order
-  ggplot(aes(x = samp_strat, fill = wealth_quintile)) +
-  geom_bar(position = "fill") +
-  xlab("Province") +
-  ylab("Percentage") +
-  ggtitle("Wealth by Province") +
-  scale_y_continuous(labels = scales::percent_format()) +
-  scale_fill_unhcr_d(guide = guide_legend(reverse = TRUE)) +  # Use UNHCR color palette
-  theme_unhcr() +
-  coord_flip()
-
-
-main_WI %>%
-  filter(Intro_07 %in% c("Refugees", "Host community"), !is.na(wealth_quintile)) %>%
-  mutate(wealth_quintile = fct_rev(wealth_quintile)) %>%  # Reverse order
-  ggplot(aes(x = Intro_07, fill = wealth_quintile)) +
-  geom_bar(position = "fill") +
-  xlab("Population Group") +
-  ylab("Percentage") +
-  ggtitle("Wealth Distribution: Refugees vs. Host Community") +
-  scale_y_continuous(labels = scales::percent_format()) +
-  scale_fill_unhcr_d(guide = guide_legend(reverse = TRUE)) +  # Use UNHCR color palette
-  theme_unhcr() +
-  coord_flip()
